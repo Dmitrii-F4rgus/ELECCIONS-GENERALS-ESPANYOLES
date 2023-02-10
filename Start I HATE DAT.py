c1d8d7bd27b037a3a61066b4a55c2cf7edac69d8
Dmitrii-F4rgus <123185694+Dmitrii-F4rgus@users.noreply.github.com>
2023-02-10 15:27:00 +0100

    Add files via upload

---
+++ Start I HATE DAT.py
@@ -0,0 +1,5 @@
+from subprocess import call
+call(["python", "I HATE DAT_(ELECCIONS).py"])
+call(["python", "I HATE DAT_(CANDIDATURES).py"])
+call(["python", "I HATE DAT_(COMUNITATS_AUTONOMES).py"])
+##call(["python", "I HATE DAT_(PROVINCIES).py"])
