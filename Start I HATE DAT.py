e59cfa6d88589516873621452acdd670ea11905b
Dmitrii-F4rgus <123185694+Dmitrii-F4rgus@users.noreply.github.com>
2023-02-15 16:16:26 +0100

    Delete Start I HATE DAT.py

--- Start I HATE DAT.py
+++
@@ -1,5 +0,0 @@
-from subprocess import call
-call(["python", "I HATE DAT_(ELECCIONS).py"])
-call(["python", "I HATE DAT_(CANDIDATURES).py"])
-call(["python", "I HATE DAT_(COMUNITATS_AUTONOMES).py"])
-##call(["python", "I HATE DAT_(PROVINCIES).py"])
