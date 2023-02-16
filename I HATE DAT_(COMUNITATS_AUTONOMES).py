79db50c64311083215a6e9b765129ff3d8e8307b
Dmitrii-F4rgus <123185694+Dmitrii-F4rgus@users.noreply.github.com>
2023-02-16 22:35:16 +0100

    Add files via upload

---
+++ I HATE DAT_(COMUNITATS_AUTONOMES).py
@@ -0,0 +1,37 @@
+import mysql.connector
+print("START")
+# Connect to the database
+cnx = mysql.connector.connect(
+    host="192.168.56.103",
+#    host="10.94.255.163",
+    user="perepi",
+    password="pastanaga",
+    database="eleccions"
+)
+cursor = cnx.cursor()
+
+# Open the file
+with open("07021911.DAT", "r") as file:
+    # Read each line of the file
+    for line in file:
+        # Strip leading and trailing whitespace from the line
+        data = line.strip()
+        # Extract the values from the data string
+        nom = data[14:64].replace("\"", "'")
+        codi_ine_prov = data[11:13]
+        codi_ine = data[9:11]
+
+        if codi_ine != "99" and codi_ine_prov == "99":
+                query = "INSERT INTO comunitats_autonomes (nom, codi_ine) VALUES (%s, %s)"
+                values = (nom, codi_ine)
+                cursor.execute(query, values)
+        else:
+            pass
+# Commit the changes
+cnx.commit()
+
+# Close the cursor and connection
+cursor.close()
+cnx.close()
+
+print("DONE")
